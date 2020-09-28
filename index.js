95994645df78bd69a3d3b674cdfbfc6f70468c08
D@rk D3vi1 <hannanhridoy@gmail.com>
2020-09-28 16:14:50 +0600

    Changed to css and Js file for nice animation

--- index.js
+++ index.js
@@ -4,14 +4,30 @@ let sampleText = "This is sample text!";
 
 let resultText = sampleText;
 
+let colors = []
+
+resultText.split("").forEach(function(el,i){
+  if(i%2 == 0){
+    colors.push('red')
+  }else if(i%3 == 0){
+    colors.push('green')
+  }else if(i%5 == 0){
+    colors.push('blue')
+  }else{
+    colors.push('black')
+  }
+})
+
 setInterval(function () {
   if (counter < resultText.length) {
     text.innerHTML += sampleText.charAt(counter++);
+    text.style.color = colors[counter]
   } else {
     resultText = resultText.split("");
     resultText.pop();
     resultText = resultText.join("");
     text.innerHTML = resultText;
+    text.style.color = colors[Math.floor(counter/2)]
     counter++;
 
     if (counter == sampleText.length * 2) {
@@ -20,4 +36,4 @@ setInterval(function () {
       text.innerHTML = "";
     }
   }
-}, 100);
+}, 500);
